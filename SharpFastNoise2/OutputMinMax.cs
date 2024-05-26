87ebd5f141dd978ef000d1ede45134f88a5d0fe2
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2024-05-26 22:07:13 +0200

    Use SSE in OutputMinMax.Apply

--- SharpFastNoise2/OutputMinMax.cs
+++ SharpFastNoise2/OutputMinMax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using System;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 
 namespace SharpFastNoise2
 {
@@ -19,8 +21,16 @@ namespace SharpFastNoise2
 
         public void Apply(float min, float max)
         {
-            Min = MathF.Min(Min, min);
-            Max = MathF.Max(Max, max);
+            if (Sse.IsSupported)
+            {
+                Min = Sse.MinScalar(Vector128.CreateScalarUnsafe(Min), Vector128.CreateScalarUnsafe(min)).ToScalar();
+                Max = Sse.MaxScalar(Vector128.CreateScalarUnsafe(Max), Vector128.CreateScalarUnsafe(max)).ToScalar();
+            }
+            else
+            {
+                Min = MathF.Min(Min, min);
+                Max = MathF.Max(Max, max);
+            }
         }
 
         public void Apply(float v)
